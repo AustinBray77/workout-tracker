fcf11233adabefef0cc85a37c32a4011b130a56a
AustinBray77 <austin.bray77@gmail.com>
2022-08-04 12:03:48 -0400

    Added Server side

---
+++ client/Components/Calendar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const Calendar = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+};
+
+export default Calendar;
