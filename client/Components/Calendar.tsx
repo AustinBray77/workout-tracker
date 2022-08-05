b7179762ba5e1893e5281e247d2124cd81a35c60
AustinBray77 <austin.bray77@gmail.com>
2022-08-05 18:27:31 -0400

    Added Functionality to Calandar Component

--- client/Components/Calendar.tsx
+++ client/Components/Calendar.tsx
@@ -1,12 +1,57 @@
+import moment from 'moment';
 import React from 'react';
-import {Text, View} from 'react-native';
+import {MaskedViewComponent, StyleSheet, Text, View} from 'react-native';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
 
-const Calendar = () => {
+const Header = (): JSX.Element => {
+  const date: Date = new Date();
+
   return (
-    <View>
-      <Text></Text>
+    <View style={styles.border}>
+      <Text style={{color: 'white'}}>
+        {moment().format('dddd, MMMM Do YYYY')}
+      </Text>
     </View>
   );
 };
+
+const Body = (): JSX.Element => {
+  return (
+    <View style={styles.main}>
+      <Text>H</Text>
+    </View>
+  );
+};
+
+const Footer = (): JSX.Element => {
+  return (
+    <View style={styles.border}>
+      <Text>H</Text>
+    </View>
+  );
+};
+
+const Calendar = (): JSX.Element => {
+  return (
+    <View style={{flex: 1}}>
+      <Header />
+      <Body />
+      <Footer />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  main: {
+    backgroundColor: 'red',
+    flex: 4,
+  },
+  border: {
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+  },
+});
 
 export default Calendar;
